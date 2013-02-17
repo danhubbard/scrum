f6352c7f23e1d2736de6f2bdd49d48ddb14fa2fe
Joe James <joe.james@alliants.co.uk>
2013-02-17 21:51:10 +0000

    add shovel items into a backlog

--- lib/scrum/backlog.rb
+++ lib/scrum/backlog.rb
@@ -4,4 +4,11 @@ class Backlog
   def initialize
     @items = []
   end
+
+  def << item
+    items << item
+  end
+
+  private
+  attr_writer :items
 end
--- test/unit/backlog_test.rb
+++ test/unit/backlog_test.rb
@@ -2,9 +2,16 @@ require './test/minitest_helper'
 require_relative '../../lib/scrum/backlog'
 
 describe Backlog do
-  describe "when created" do
-    it "should be empty" do
-      assert_empty Backlog.new.items
+  let(:backlog) { Backlog.new }
+
+  it "should be empty" do
+    assert_empty backlog.items
+  end
+
+  describe 'with items' do
+    it "should not be empty" do
+      backlog << mock
+      refute_empty backlog.items
     end
   end
 end
